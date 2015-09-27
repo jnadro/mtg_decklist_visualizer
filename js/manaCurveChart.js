d4d38b3b97653e07c1b4573ab8a02bcaad1db53d
jnadro <jason.nadro@gmail.com>
2015-09-27 15:11:25 -0500

    Adding x-axis to mana curve bar chart.
    - Remove the display none that was causing the x-axis to not show up.
    - Draw the x-axis after the bars in the bar chart.  This is done because
    the way I am selecting elements was causing the x-axis to get deleted.

--- js/manaCurveChart.js
+++ js/manaCurveChart.js
@@ -18,6 +18,7 @@ function manaCurveChart() {
   function chart(selection) {
     selection.each(function(data) {
 
+      // Create the svg element we will render into.
       var svg = d3.select(this).append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
@@ -27,28 +28,34 @@ function manaCurveChart() {
       x.domain(data.map(function(d, i) { return i; }));
       y.domain([0, d3.max(data, function(d) { return d; })]);
 
-/*
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-*/
+      // Create a group for each bar in the dataset.
+      // Must create a group that way we can later append 
+      // the rect and text as a child.
+      // svg text will not show up when appended to a rect.
       var bar = svg.selectAll("g")
           .data(data)
         .enter().append("g")
           .attr("transform", function(d, i) { return "translate(" + x(i) + ",0)"; });
 
+      // Append the rect for each bar in the bar chart.
       bar.append("rect")
           .attr("class", "bar")
           .attr("width", x.rangeBand())
           .attr("y", function(d) { return y(d); })
           .attr("height", function(d) { return height - y(d); });
 
+      // Append the count above each rectangle in the bar chart.
       var padding = 4;
       bar.append("text")
             .attr("x", function(d, i) { return x.rangeBand() / 2; })
             .attr("y", function(d, i) { return y(d) - padding; })
-            .text(function(d) { if (d !== 0) return d; });      
+            .text(function(d) { if (d !== 0) return d; });
+
+      // Append X-Axis
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
     });
   }
 
